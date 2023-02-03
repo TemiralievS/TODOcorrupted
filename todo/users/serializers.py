63ae545169c5c56a09b4b9a9867972c4ce9ea201
TemiralievS <okillington12@gmail.com>
2023-02-03 19:02:48 +0300

    lesson_1 User and API

--- todo/users/serializers.py
+++ todo/users/serializers.py
@@ -1,8 +1,8 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import ModelSerializer
 from .models import User
 
 
-class AuthorModelSerializer(HyperlinkedModelSerializer):
+class UserModelSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('user_name', 'first_name', 'last_name', 'email', 'birthday_year')
--- todo/users/views.py
+++ todo/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.renderers import JSONRenderer
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+
+from .models import User
+from .serializers import UserModelSerializer
+
+
+class UserModelViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer

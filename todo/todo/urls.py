f87a002df4f3b547561dccb0d8faca9b9c24e5a0
TemiralievS <okillington12@gmail.com>
2023-02-11 19:26:31 +0300

    lesson_3 todo and  projects

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from rest_framework.routers import DefaultRouter
 
+from todoapp.views import ToDoViewSet, ProjectViewSet
 from users.views import UserModelViewSet
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
+router.register('todos', ToDoViewSet)
+router.register('projects', ProjectViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),

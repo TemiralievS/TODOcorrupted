422f6ed46d719d62d44ba194b51645981739fa92
TemiralievS <101124591+TemiralievS@users.noreply.github.com>
2023-02-17 11:57:48 +0300

    lesson_3 todo and  projects (#3)
    lesson_3

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users',
     'corsheaders',
+    'users',
+    'todoapp',
 ]
 
 MIDDLEWARE = [
